adbaa1da874121bac1a6bdb650d1da0ff5129021
Al Jachimiak <aljachimiak@gmail.com>
2019-01-03 08:27:43 -0500

    adds failing outcome for isDivisibleByThree test

--- tests/index_test.js
+++ tests/index_test.js
@@ -1,6 +1,14 @@
 import test from 'ava';
 import FizzBuzz from '../index';
 
-test('IsDivisibleByThree functions passes', t => {
-	t.is(FizzBuzz.isDivisibleByThree(4), true, 'isDivisibleByThree failed with value 3');
+test('IsDivisibleByThree function passes', t => {
+	t.is(FizzBuzz.isDivisibleByThree(3), true, 'isDivisibleByThree failed with value 3');
+});
+
+test('IsDivisibleByThree function fails', t => {
+	const values = [1, 2, 4];
+
+	values.forEach(val => {
+		t.is(FizzBuzz.isDivisibleByThree(val), false, `isDivisibleByThree failed with value ${val}`);
+	});
 });
