c282e3bdf9e05ef3603bff8e49c9d10398b96927
Al Jachimiak <aljachimiak@gmail.com>
2019-01-03 08:33:06 -0500

    adds tests for isDivisibleByFive

--- tests/index_test.js
+++ tests/index_test.js
@@ -1,14 +1,34 @@
 import test from 'ava';
 import FizzBuzz from '../index';
 
-test('IsDivisibleByThree function passes', t => {
-	t.is(FizzBuzz.isDivisibleByThree(3), true, 'isDivisibleByThree failed with value 3');
+test('IsDivisibleByThree function returns true', t => {
+	const values = [3, 6, 9];
+
+	values.forEach(val => {
+		t.is(FizzBuzz.isDivisibleByThree(val), true, `isDivisibleByThree failed with value ${val}`);
+	});
 });
 
-test('IsDivisibleByThree function fails', t => {
+test('IsDivisibleByThree function returns false', t => {
 	const values = [1, 2, 4];
 
 	values.forEach(val => {
 		t.is(FizzBuzz.isDivisibleByThree(val), false, `isDivisibleByThree failed with value ${val}`);
+	});
+});
+
+test('IsDivisibleByFive function returns true', t => {
+	const values = [5, 10, 15];
+
+	values.forEach(val => {
+		t.is(FizzBuzz.isDivisibleByFive(val), true, `isDivisibleByFive failed with value ${val}`);
+	});
+});
+
+test('IsDivisibleByFive function returns false', t => {
+	const values = [1, 2, 3, 4, 6, 7, 8, 9];
+
+	values.forEach(val => {
+		t.is(FizzBuzz.isDivisibleByFive(val), false, `isDivisibleByFive failed with value ${val}`);
 	});
 });
