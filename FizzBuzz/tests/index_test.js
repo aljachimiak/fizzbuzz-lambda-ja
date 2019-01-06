d97e9a4b42e932e55c7346c001b8ace2d2bbf603
Al Jachimiak <aljachimiak@gmail.com>
2019-01-05 21:03:35 -0500

    adds more integratin tests

--- FizzBuzz/tests/index_test.js
+++ FizzBuzz/tests/index_test.js
@@ -3,7 +3,9 @@ import index from '../index';
 
 test('Index Test', t => {
 	const responses = {
-		number_100: null
+		number_100: null,
+		missing_number: null,
+		number_999999: null
 	};
 	return Promise.resolve(null)
 		.then(() => {
@@ -16,7 +18,33 @@ test('Index Test', t => {
 			return null;
 		})
 		.then(() => {
+			return index.handler({
+				queryStringParameters: {}
+			});
+		}) 
+		.then(res => {
+			responses.missing_number = JSON.parse(res.body);
+			return null;
+		})
+		.then(() => {
+			return index.handler({
+				queryStringParameters: {number: '999999'}
+			});
+		}) 
+		.then(res => {
+			responses.number_999999 = JSON.parse(res.body);
+			return null;
+		})
+		.then(() => {
 			const data = responses.number_100.data;
-			t.is(data.length, 100);
+			t.is(data.length, 100, "Index returns incorrect data length when 100 is reuested");
+		})
+		.then(() => {
+			const data = responses.missing_number.data;
+			t.is(data.length, 100, "Index returns incorrect data length when no number is requested");
+		})
+		.then(() => {
+			const data = responses.number_999999.data;
+			t.is(data.length, 10000, "Index returns incorrect data length when a too large number is requested");
 		});
 });
